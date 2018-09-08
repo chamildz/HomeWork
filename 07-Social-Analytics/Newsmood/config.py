cf5b7893f99144c86f3e1c498e0b0c1afa862260
Chamil De Zoysa <chamil@outlook.com>
2018-09-08 13:23:02 -0500

    Newsmood Assignment Code

---
+++ 07-Social-Analytics/Newsmood/config.py
@@ -0,0 +1,4 @@
+consumer_key = "gnFzdK7gzmDhGqJLWPaQM7xli"
+consumer_secret = "somTfUhhKlIgLLXbtpzXjYJtjmPvd3DWzclLNR2xqTRaiWlJYm"
+access_token = "1029920597822734336-ar03otVg12sirOeFbl6orgr4pJQNlg"
+access_token_secret = "3s2qCvy97KiIcKceXDcYpw561qW9LBwYURe4T8tvuLo3K"
